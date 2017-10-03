01490e63bce423c8cfcf7df6ab59041cc6c3ca81
Simen Bjerkeset Andersen <simen-bandersen@hotmail.no>
2017-10-03 09:45:14 +0200

    final
    added comments

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 (function() {
 
+  // Global variables
   let $employees = [];
   let $htmlOutput = "<ul>";
   let $overlayOutput;
   let $active;
 
+  // Add information about employees to the main screen
   function createEmployee(index) {
 
+    // HTML string with employee information
     $htmlOutput += "<div class='employee' data-index=" + index + ">" +
     "<img src=" + $employees[index].img + ">" +
     "<div class='name'>" + $employees[index].name + "</div>" +
@@ -15,10 +18,13 @@
     $htmlOutput += "</div>";
   }
 
+  // Add additional information about employees to the modal window
   function createModalEmployee(index) {
-    $active = index;
-    console.log($active);
 
+    // Keep track of the current employee
+    $active = index;
+
+    // HTML string with employee information
     $overlayOutput = "<img src=" + $employees[index].img + ">" +
     "<div class='name'>" + $employees[index].name + "</div>" +
     "<div class='username'>" + $employees[index].username + "</div></br>" +
@@ -27,13 +33,16 @@
     "</br><div class='phone'>" + $employees[index].phone + "</div>" +
     "<div class='street'>" + $employees[index].street + "</div>" +
     "<div class='state'>" + $employees[index].state + "</div>" +
+    "<div class='country'>" + $employees[index].country + "</div>" +
     "<div class='postcode'>" + $employees[index].postcode + "</div></br>" +
     "<div class='birthdate'>" + $employees[index].birthdate + "</div>";
 
+    // Append the HTML string
     $("#modalWindow").append($overlayOutput);
 
     $htmlOutput = "<ul>";
 
+    // Show/hide buttons depending on which employee is being shown
     if (index == ($employees.length - 1)) {
       $("#next").hide();
       $("#prev").show();
@@ -46,23 +55,27 @@
     }
   }
 
+  // Close the modal window
   $("#close").click(function() {
     $("#overlay").css("display", "none");
     $("#modalWindow").html("");
   });
 
+  // Show the next modal window employee
   $("#next").click(function() {
     $("#modalWindow").html("");
     $("#overlay").css("display", "block");
     createModalEmployee($active+1);
   });
 
+  // Show the previous modal window employee
   $("#prev").click(function() {
     $("#modalWindow").html("");
     $("#overlay").css("display", "block");
     createModalEmployee($active-1);
   });
 
+  // Search for employee based on name and username
   $("#submitButton").click(function(event) {
     event.preventDefault();
 
@@ -81,11 +94,13 @@
 
   });
 
+  // AJAX call
   $.ajax({
     url: 'https://randomuser.me/api/?results=12&nat=gb',
     dataType: 'json',
     success: function(data) {
 
+      // Create an object for each employee
       for (let i = 0; i < data.results.length; i++) {
         let $employee = {};
 
@@ -102,10 +117,12 @@
         $employee.country = data.results[i].nat;
         $employee.birthdate = data.results[i].dob;
 
+        // Adds the employee object to the end of the global employees array
         $employees.push($employee);
 
       }
 
+      // Create the html for all the employees
       for (let i = 0; i < $employees.length; i++) {
 
         createEmployee(i);
@@ -114,8 +131,10 @@
 
       $htmlOutput += "</ul>";
 
+      // Show the employees
       $("#employees").html($htmlOutput);
 
+      // Open the overlay/modal window
       $(".employee").click(function(event) {
         $("#overlay").css("display", "block");
 
